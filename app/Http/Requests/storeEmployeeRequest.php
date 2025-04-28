aa62b3301e85e8fd2511a1568bec7f90804222b1
AehamAlHaeak <www.haeak@gmal.com>
2025-04-28 21:05:03 +0300

    add the employe controller

--- app/Http/Requests/storeEmployeeRequest.php
+++ app/Http/Requests/storeEmployeeRequest.php
@@ -33,7 +33,8 @@ class storeEmployeeRequest extends FormRequest
             "start_time"=>"required",
             "work_hours"=>"required|integer|max:10",
             "workable_type"=>"in:Warehouse,DistributionCenter",
-           "workable_id"=>"integer"
+            "workable_id"=>"integer"
         ];
     }
 }
+//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2xvZ2luX2VtcGxveWUiLCJpYXQiOjE3NDU4NjMwMzksImV4cCI6MTc0NTg2NjYzOSwibmJmIjoxNzQ1ODYzMDM5LCJqdGkiOiJsaHpldmdjdm53NXdWV1c2Iiwic3ViIjoiMiIsInBydiI6IjBiNWY3ODZiM2NhMTg5MzQ1M2NiZGJmYjJkZWU0YmEyZmQzMzJhZmMiLCJpZCI6MiwiZW1haWwiOiJ3d3cuYXNpa0BnbWFpbC5jb20iLCJwaG9uZV9udW1iZXIiOjk4NzY1NDMyMX0.Q5_mTuSsi2jRjjQTS2BJoYZ5ZOz1e98o7chl1UmGBi4
