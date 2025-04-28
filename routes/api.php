94b87dcc04f0b0c20255003d606ebc30cc1f4a2a
AehamAlHaeak <152405140+AehamAlHaeak@users.noreply.github.com>
2025-04-28 19:02:56 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,9 +11,9 @@ use App\Http\Controllers\SuperAdmenController;
 Route::post("create",[SuperAdmenController::class, "create_new_item"]);
 Route::post("create_new_specification",[SuperAdmenController::class, "create_new_specification"]);
 Route::post("create_new_employe",[SuperAdmenController::class,"create_new_employe"]);
-Route::post("login_employee",[SuperAdmenController::class,'login_employee']);
+Route::post("login_employee",[EmployeesController::class,'login_employee']);
 Route::middleware('auth.api:employee')->group(function () {
-    Route::post('logout_employee',[SuperAdmenController::class,'logout_employee']);
+    Route::post('logout_employee',[EmployeesController::class,'logout_employee']);
 });
 
 
