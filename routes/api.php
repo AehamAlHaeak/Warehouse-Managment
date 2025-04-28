61189025d38251614dbdd2dc61367d347dbaa9f1
AehamAlHaeak <www.haeak@gmal.com>
2025-04-28 21:06:46 +0300

    some movments in routes and delete unwanted routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\EmployeController;
 use App\Http\Controllers\IDUController;
 use App\Http\Controllers\UserContruller;
 use App\Http\Controllers\SuperAdmenController;
@@ -11,7 +12,7 @@ use App\Http\Controllers\SuperAdmenController;
 
 Route::controller(SuperAdmenController::class)->group(function () {
     Route::post("create_new_warehouse", "create_new_warehouse");
-    Route::post("create_new_specification",[SuperAdmenController::class, "create_new_specification"]);
+    Route::post("create_new_specification", "create_new_specification");
     Route::post("create_new_employe","create_new_employe");
     Route::post("create_new_distribution_center","create_new_distribution_center");
     Route::post("create_new_vehicle","create_new_vehicle");
@@ -21,9 +22,9 @@ Route::controller(SuperAdmenController::class)->group(function () {
 });
 //create_new_garage
 
-Route::post("login_employee",[SuperAdmenController::class,'login_employee']);
+Route::post("login_employe",[EmployeController::class,'login_employe']);
 Route::middleware('auth.api:employee')->group(function () {
-    Route::post('logout_employee',[EmployeesController::class,'logout_employee']);
+    Route::post('logout_employe',[SuperAdmenController::class,'logout_employe']);
 });
 
 
