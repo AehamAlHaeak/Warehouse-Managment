9bd69737c4912391753db3012e2296c3d9433e23
AehamAlHaeak <www.haeak@gmal.com>
2025-05-06 15:08:27 +0300

    add a public serveice show supported products

--- app/Models/DistributionCenter.php
+++ app/Models/DistributionCenter.php
@@ -13,13 +13,16 @@ class DistributionCenter extends Model
     public function bill(){
         return $this->hasMany(Bill::class);
     }
-
-    public function Products(){
-    return $this->hasMany(distribution_center_Product::class,"distribution_center_id");
+    
+    public function public_details_about_products(){
+    return $this->hasMany(Distribution_center_Product::class,"distribution_center_id");
     }
 
 
     public function employes(){
         return $this->morphMany(Employe::class,"workable");
+    }
+    public function supported_roduct(){
+        return $this->belongsToMany(product::class,'distribution_center__product');
     }
 }
