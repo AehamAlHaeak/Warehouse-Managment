d87a491b107b7a111eea5ec3bb184766962a4789
Majd212003 <majdmajood0035@gmail.com>
2025-05-16 14:33:53 +0300

    the mission

---
+++ app/Jobs/importing_op_prod.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Models\Import_operation_product;
+
+class importing_op_prod implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    public $import_operation;
+    public $validated_products;
+
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($import_operation, $validated_products)
+    {
+           $this->import_operation = $import_operation;
+           $this->validated_products = $validated_products;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+    // check if the produts is availabe(existable)
+        if (!empty($this->validated_products)) {
+            foreach ($this->validated_products as $product) {
+                // إضافة import_operation_id إلى المنتج
+                $product['import_operation_id'] = $this->import_operation->id;
+
+                // إنشاء السجل الجديد في جدول import_operation_product
+                Import_operation_product::create($product);
+                Log::info("Product added to import_operation_product: " . json_encode($product));
+            }
+        }
+    }
+}
