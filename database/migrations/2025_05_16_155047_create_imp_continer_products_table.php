0132bda4e13782ba964ae539e294930d55f4745f
AehamAlHaeak <www.haeak@gmal.com>
2025-05-17 11:25:42 +0300

    add realtion table betwen imp_prod and imp_contin

---
+++ database/migrations/2025_05_16_155047_create_imp_continer_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('imp_continer_products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger("imp_op_cont_id");
+            $table->foreign("imp_op_cont_id")->references("id")->on("import_op_containers");
+            $table->unsignedBigInteger("imp_op_product_id");
+            $table->foreign("imp_op_product_id")->references("id")->on("import_operation_product");
+            $table->integer("load")->default(0);//unable to modefi
+        
+            $table->integer("sold_load")->default(0);
+             
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('imp_continer_products');
+    }
+};
