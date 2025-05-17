7057e4d10d0f0421d43d4ffc0f9b5c0b45acf5ef
AehamAlHaeak <www.haeak@gmal.com>
2025-05-17 11:28:08 +0300

    add details to continer

---
+++ database/migrations/2025_05_16_183149_create_sell_detail_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sell_detail', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger("transfer_details_id");
+            $table->foreign("transfer_details_id")->references("id")->on("transfer__details");
+            $table->integer("sold_load");
+            $table->unsignedBigInteger("imp_cont_prod_id");
+            $table->foreign("imp_cont_prod_id")->references("id")->on("imp_continer_products");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sell_detail');
+    }
+};
---
+++ database/migrations/2025_05_16_183732_create_reject_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reject_details', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger("employe_id");
+            $table->foreign("employe_id")->references("id")->on("employes");
+            $table->integer("rejected_load");
+            $table->unsignedBigInteger("imp_cont_prod_id");
+            $table->foreign("imp_cont_prod_id")->references("id")->on("imp_continer_products");
+            $table->text("why");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reject_details');
+    }
+};
