0dbd93a9ab0a1aa2a6dd5d3275c913c89836429c
AehamAlHaeak <www.haeak@gmal.com>
2025-05-17 12:00:16 +0300

    add using some classes

--- app/Traits/TransferTrait.php
+++ app/Traits/TransferTrait.php
@@ -3,12 +3,15 @@
 
 namespace App\Traits;
 
-use App\Models\Import_jop_product;
-use App\Models\Import_operation_product;
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Vehicle;
 use App\Models\Transfer;
 use App\Models\TransferDetails;
-use App\Models\Vehicle;
-use Carbon\Carbon;
+use App\Models\DistributionCenter;
+use App\Models\Import_jop_product;
+use App\Models\Import_operation_product;
+
 trait TransferTrait
 {
 
