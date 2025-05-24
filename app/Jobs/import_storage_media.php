c617bf2b38a272ead741a380a7d74404487a0fb2
AehamAlHaeak <152405140+AehamAlHaeak@users.noreply.github.com>
2025-05-24 13:27:36 +0300

    Update import_storage_media.php

--- app/Jobs/import_storage_media.php
+++ app/Jobs/import_storage_media.php
@@ -18,51 +18,57 @@ class import_storage_media implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     use AlgorithmsTrait;
-    protected $import_operation_id;
+   protected $import_operation_id;
     protected $storage_media;
-    public function __construct($import_operation_id, $storage_media)
+    public function __construct($import_operation_id,$storage_media)
     {
-        $this->import_operation_id = $import_operation_id;
-        $this->storage_media = $storage_media;
+        $this->import_operation_id=$import_operation_id;
+        $this->storage_media=$storage_media;
     }
-    public function fetch_sections($related_storage_media)
-    {
-        //  $sections=$related_storage_media
+    public function fetch_sections($related_storage_media){
+    //  $sections=$related_storage_media
     }
 
-    public function ord_sections() {}
+    public function ord_sections(){
 
-    public function distribute_storage_media() {}
+    }
+
+     public function distribute_storage_media(){
+
+     }
 
 
     public function handle(): void
     {
-        $created_storage_units = [];
-        foreach ($this->storage_media as $storage_element) {
-            $section = Section::find($storage_element["section_id"]);
-            $section_empty_posetions = $section->posetions()->whereNull('storage_media_id')->get();
-            $empty_count = $section_empty_posetions->count();
-            $parent_storage_media = Storage_media::find($storage_element["storage_media_id"]);
-            for ($count = 0; $count < min($storage_element["quantity"], $empty_count); $count++) {
+        $created_storage_units=[];
+         foreach($this->storage_media as $storage_element){
+           $section=Section::find($storage_element["section_id"]);
+           $section_empty_posetions = $section->posetions()->whereNull('storage_media_id')->get();
+           $empty_count = $section_empty_posetions->count();
+          $parent_storage_media=Storage_media::find($storage_element["storage_media_id"]);
+            for ($count = 0; $count < min($storage_element["quantity"], $empty_count); $count++){
+               
 
-
-
-                $storage_unit = Import_op_storage_md::create([
-                    "storage_media_id" => $storage_element["storage_media_id"],
-                    "import_operation_id" => $this->import_operation_id,
-
-                ]);
-
-                $section_empty_posetions[$count]->storage_media_id = $storage_unit->id;
-                $section_empty_posetions[$count]->save();
-
-                $storage_unit->num_floors = $parent_storage_media->num_floors;
-                $storage_unit->num_classes = $parent_storage_media->num_classes;
-                $storage_unit->num_positions_on_class = $parent_storage_media->num_positions_on_class;
-
+                
+              $storage_unit=Import_op_storage_md::create([
+               "storage_media_id"=>$storage_element["storage_media_id"],
+               "import_operation_id"=> $this->import_operation_id,
+                
+            ]);
+            
+                 $section_empty_posetions[$count]->storage_media_id=$storage_unit->id;
+                 $section_empty_posetions[$count]->save();
+              
+               $storage_unit->num_floors=$parent_storage_media->num_floors;
+               $storage_unit->num_classes=$parent_storage_media->num_classes;
+               $storage_unit->num_positions_on_class=$parent_storage_media->num_positions_on_class;
+               
                 $created_storage_units[$storage_unit->id] = $storage_unit;
-                $this->create_postions("App\\Models\\Positions_on_sto_m", $storage_unit, "imp_op_stor_id");
+           $this->create_postions("App\\Models\\Positions_on_sto_m",$storage_unit,"imp_op_stor_id");
+          
             }
-        }
+
+         }
+
     }
 }
