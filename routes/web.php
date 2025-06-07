41a6e6bd102cf37b301c2d5884aa53b58d52a723
AehamAlHaeak <www.haeak@gmal.com>
2025-06-08 01:28:09 +0300

    some feature in supplier configration added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\IDUController;
+use App\Http\Controllers\EmployeController;
 use App\Http\Controllers\WarehouseController;
 use App\Http\Controllers\SuperAdmenController;
 /*
